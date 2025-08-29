ec5f0b0973fad5649478fcf9f651c1ab27da027d
PumaSgnd <bomavan12@gmail.com>
2025-08-29 13:27:59 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
-import PrivacyPolicy from "./components/PrivacyPolicy";
-import Terms from "./components/Terms";
+import PrivacyPolicy from "./components/privacy-policy";
+import Terms from "./components/TermsOfService";
 
 function App() {
   return (
@@ -15,15 +15,15 @@ function App() {
           gap: "15px" 
         }}>
           <Link to="/" style={{ color: "white", textDecoration: "none" }}>Home</Link>
-          <Link to="/privacy" style={{ color: "white", textDecoration: "none" }}>Privacy Policy</Link>
-          <Link to="/terms" style={{ color: "white", textDecoration: "none" }}>Terms</Link>
+          <Link to="/privacy-policy" style={{ color: "white", textDecoration: "none" }}>Privacy Policy</Link>
+          <Link to="/terms-of-service" style={{ color: "white", textDecoration: "none" }}>Terms</Link>
         </nav>
 
         {/* Routes */}
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/privacy" element={<PrivacyPolicy />} />
-          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<Terms />} />
         </Routes>
       </div>
     </Router>
