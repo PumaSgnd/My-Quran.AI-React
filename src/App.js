878396b2353fc8e10347ec5707de6a70586d707e
PumaSgnd <bomavan12@gmail.com>
2026-07-02 20:26:02 +0700

    Public Image Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Home from "./components/Home";
 import PrivacyPolicy from "./components/privacy-policy";
 import Terms from "./components/TermsOfService";
 import DataDeletion from "./components/DataDeletion"
